71998f2692c82a6a9aa382d96fbe35bcecbe6a2b
Iván Martín <ivanmartinga@gmail.com>
2020-10-11 16:25:06 +0200

    delete files

--- common/src/lib.rs
+++
@@ -1,8 +0,0 @@
-//! A crate to hold types and functions common to all rustpython components.
-
-pub mod borrow;
-pub mod float_ops;
-pub mod hash;
-pub mod lock;
-pub mod rc;
-pub mod str;
--- jit/tests/lib.rs
+++
@@ -1,5 +0,0 @@
-#[macro_use]
-mod common;
-mod float_tests;
-mod int_tests;
-mod misc_tests;
